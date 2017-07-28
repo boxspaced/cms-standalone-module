f27046f22aff7f0587963e8b8946f35d734a9811
rw0600 <rw0600@gmail.com>
2017-07-28 12:52:27 +0100

    Force https and admin nav events/listeners

--- src/Controller/StandaloneControllerFactory.php
+++ src/Controller/StandaloneControllerFactory.php
@@ -7,23 +7,18 @@ use Boxspaced\CmsStandaloneModule\Controller\StandaloneController;
 use Boxspaced\CmsStandaloneModule\Service\StandaloneService;
 use Boxspaced\CmsAccountModule\Service\AccountService;
 use Zend\Log\Logger;
-use Boxspaced\CmsCoreModule\Controller\AbstractControllerFactory;
 
-class StandaloneControllerFactory extends AbstractControllerFactory implements FactoryInterface
+class StandaloneControllerFactory implements FactoryInterface
 {
 
     public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $controller = new StandaloneController(
+        return new StandaloneController(
             $container->get(StandaloneService::class),
             $container->get(AccountService::class),
             $container->get(Logger::class),
             $container->get('config')
         );
-
-        $this->adminNavigationWidget($controller, $container);
-
-        return $this->forceHttps($controller, $container);
     }
 
 }
